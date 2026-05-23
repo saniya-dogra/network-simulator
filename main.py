a99c40966465af70790819b5e7ad4484491aff2e
saniya-dogra <saniyadogra2@gmail.com>
2026-05-23 14:40:45 +0530

    Modify main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from test_cases.test_physical import physical_layer_demo
 from test_cases.test_datalink import datalink_demo
+from test_cases.test_network import network_demo
 
 print("\n========== PHYSICAL LAYER ==========")
 physical_layer_demo()
 
 print("\n========== DATA LINK LAYER ==========")
 datalink_demo()
+
+
+print("\n========== NETWORK LAYER ==========")
+network_demo()
+
+
